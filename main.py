72411bddf89d010f1aa632d6835aef9c651108ff
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-08-17 17:50:18 +0100

    Added a workflow and some test code to main

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Main file for game"""
+
+import tcod
+
+
+def main() -> None:
+    """Test function to ensure all is working"""
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
