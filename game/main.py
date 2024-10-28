539971a9edc32203fc6233fa17895f2e65733701
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-28 15:38:33 +0000

    Moved files into game directory

--- game/main.py
+++ game/main.py
@@ -14,7 +14,7 @@ def main() -> None:
     player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
-        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+        "assets/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
