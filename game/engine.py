1646c23f9a747664c7055e59507191bfa80038ed
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-12-17 16:41:58 +0000

    Created engine file to host the engine class

---
+++ game/engine.py
@@ -0,0 +1,40 @@
+"""File for the engine class."""
+
+from typing import Set, Iterable, Any
+
+from tcod.context import Context
+from tcod.console import Console
+
+from actions import EscapeAction, MovementAction
+from entity import Entity
+from input_handlers import EventHandler
+
+
+class Engine:
+    """Class for drawing map and entities, and handling player input."""
+
+    def __init__(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
+        self.entities = entities
+        self.event_handler = event_handler
+        self.player = player
+
+    def handle_events(self, events: Iterable[Any]) -> None:
+        for event in events:
+            action = self.event_handler.dispatch(event)
+
+            if action is None:
+                continue
+
+            if isinstance(action, MovementAction):
+                self.player.move(dx=action.dx, dy=action.dy)
+
+            elif isinstance(action, EscapeAction):
+                raise SystemExit()
+
+    def render(self, console: Console, context: Context) -> None:
+        for entity in self.entities:
+            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+
+        context.present(console)
+
+        console.clear()
