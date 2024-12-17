ebfa4dff5e052b2f5323f08e7fc54aa9204011ca
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-12-17 17:24:04 +0000

    Updated actions

--- game/actions.py
+++ game/actions.py
@@ -1,21 +1,46 @@
 """File that contains possible user actions."""
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 
 class Action:
-    """Superclass used for any action."""
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """Perform this action with the objects needed to determine its scope.
+
+        `engine` is the scope this action is being performed in.
+
+        `entity` is the object performing the action.
+
+        This method must be overridden by Action subclasses.
+        """
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    """Subclass of action, used for presses of the Esc key."""
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 
 class MovementAction(Action):
-    """Subclass of action, used for presses of movement keys."""
-
     def __init__(self, dx: int, dy: int):
         super().__init__()
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds.
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
+
+        entity.move(self.dx, self.dy)
